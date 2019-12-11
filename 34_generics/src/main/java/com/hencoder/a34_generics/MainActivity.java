f15c5357256897b85d6874fa8415b82d86cd88e5
Seachal <seachalzhang@gmail.com>
2019-12-11 15:20:50 +0800

    修改配置文件

--- 34_generics/src/main/java/com/hencoder/a34_generics/MainActivity.java
+++ 34_generics/src/main/java/com/hencoder/a34_generics/MainActivity.java
@@ -1,28 +1,20 @@
 package com.hencoder.a34_generics;
 
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.gson.reflect.TypeToken;
 import com.hencoder.a34_generics.fruit.Apple;
 import com.hencoder.a34_generics.fruit.Fruit;
-import com.hencoder.a34_generics.fruit.Watermelon;
-import com.hencoder.a34_generics.shop.FruitShop;
-import com.hencoder.a34_generics.shop.NormalShop;
 import com.hencoder.a34_generics.shop.Shop;
 
-import java.lang.reflect.Array;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     List<Shop<Apple>> appleShopList = new ArrayList<Shop<Apple>>(){};
 
-type projection
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
